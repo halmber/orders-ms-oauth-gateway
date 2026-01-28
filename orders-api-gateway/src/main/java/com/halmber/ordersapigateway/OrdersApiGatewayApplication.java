9db74fdd0059c97951c900b2539e75d8352deb8c
halmber <rogozyanskiy.bogdan@gmail.com>
2026-01-29 00:07:10 +0200

    chore: add gateway repo

---
+++ orders-api-gateway/src/main/java/com/halmber/ordersapigateway/OrdersApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.halmber.ordersapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersApiGatewayApplication.class, args);
+    }
+
+}
